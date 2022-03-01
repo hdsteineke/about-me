5f7727eafb64a9424aaf4d5884ec7691fabb7576
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-03-01 14:31:26 -0800

    Working Button!

--- app.js
+++ app.js
@@ -2,6 +2,18 @@
 
 // grab DOM elements
 
+const showButton = document.getElementById('button');
+
+const animalDiv = document.getElementById('animal');
+
+showButton.addEventListener('click', () => {
+
+    animalDiv.classList.remove('hidden');
+    showButton.classList.add('hidden');
+
+
+}
+);
 // set event listeners 
     // get user input
     // use user input to update state 
